b04b2ec5e04d3f2476344c846039059d1b6ee533
AutoHubEngineering <vetle_99@live.no>
2024-05-29 11:12:26 +0200

    Create Arduino_program.ino
    Create glabal variables

---
+++ Arduino_program/Arduino_program.ino
@@ -0,0 +1,21 @@
+
+#define PI 3.14159265358979323
+
+float CurrentRad;             //The radian posistion of the first servo
+//Tweaking Variables
+float PhaseShift = PI / 2;    //Shifting the phase with the distance between each arm/servo
+float Hz = 0.5;               //The frequency of the wave 
+short Amplitude = 40;         //The ampitude in degrees
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
