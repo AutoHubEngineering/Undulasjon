c9d8733a88aaa595f8a4496534b76e1f6ecad9cb
AutoHubEngineering <vetle_99@live.no>
2024-05-31 14:57:35 +0200

    Current sensor in tweaking
    Added output of current, voltage and power usage.

--- Arduino_program/Arduino_program.ino
+++ Arduino_program/Arduino_program.ino
@@ -1,48 +1,103 @@
 #include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
 
 #define PI 3.14159265358979323
-#define NumServos 6           //The number of servos used
+#define NumServos 6             //The number of servos used
+#define PhaseShift PI / 2       //Shifting the phase with the distance between each arm/servo
+#define StepTime 8000           //The time of each step in mS
+#define Steps 10                //The number of steps that will be executed 
+
+
 //Tweaking constants
-#define Hz 0.5                //The frequency of the wave 
-#define PhaseShift PI / 2     //Shifting the phase with the distance between each arm/servo
-#define Amplitude 40          //The ampitude in degrees
+bool HzTweaking = false;        //True when Hz tweaking is needed
+#define HzStart 0.4             //The starting frequency of the wave 
+#define HzStepIncrease 0.02     //The Hz increase for each step
+bool AmplitudeTweaking = false; //True when Amplitude tweaking is needed
+#define AmplitudeStart 30       //The starting ampitude in degrees
+#define AmplitudeStepIncrease 1 //The Hz increase for each step
 
-float CurrentRad;             //The radian posistion of the first servo
 
+//Global variables
+float CurrentRad;               //The radian posistion of the first servo
+float Hz = HzStart;
+float Amplitude = AmplitudeStart;
+unsigned long StepTimer;
+int StepCounter = 1;
 
+//Setting up current sensor
+Adafruit_INA219 ina219;
+
+//Setting up the servos 
 Servo servo[NumServos];
 int servoPin[NumServos] = {2, 3, 4, 5, 6, 7};       //The pin to the servos on the "Arduino Mega 2560". The first servo is servo[0].
 
+
+//The servo angle calculator function. Returns the next angle to the servo
 int servoAngleCalc(int servoNumber){
   float servoRad = CurrentRad + (servoNumber * PhaseShift);     //Calculate the radian position to the servo
   double armPos = sin(servoRad);                                //Calculates the arm position. How far the arm are from the equalibrium line. 
   int servoAngle = map(armPos, -1, 1, -Amplitude, Amplitude);   //Mapping from the sin value to the rigth degree for the servo
-  //servo[servoNumber].write(servoAngle);                         //Writes to the angle to the servo
   return servoAngle;
 }
-
-
-
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting up the system...");
-
   //Attach pins to servos
   for(int i = 0; i < NumServos; i++){
-    servo[i].attach(i);
+    servo[i].attach(servoPin[i]);
   }
-
-
+  //Starting the step timer and writes the first step
+  StepTimer = millis() + StepTime;
+  Serial.print("The" + String(StepCounter) + "th step:   Hz: " + String(Hz)+"   Amplitude: " + String(Amplitude));
 }
 
 void loop() {
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
   CurrentRad = 2 * PI * millis() * Hz * 0.001;      //Sinus wave generator
+
+  if(millis() > StepTimer){
+    StepCounter++;
+    if(StepCounter > Steps){
+      Serial.println("Sequence tweaking test finished...");
+      delay(999999999);
+    }
+    Serial.print("The" + String(StepCounter) + "th step:  ");
+    StepTimer = millis() + StepTime;
+    if(HzTweaking){
+      Hz = Hz + HzStepIncrease;
+      Serial.print("Hz: " + String(Hz));
+    }
+    if(AmplitudeTweaking){
+      Amplitude = Amplitude + AmplitudeStepIncrease;
+      Serial.print("Amplitude: " + String(Amplitude));
+    }
+    Serial.println(" ");
+  }
+
 
   for(int i = 0; i < NumServos; i++){
     servo[i].write(servoAngleCalc(i));
   }
 
+  //Output the power consumption
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  Serial.println("");
+  
   delay(10);
 }
