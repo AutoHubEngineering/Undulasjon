93c336a1a76bb7d5fef9f6cab10b7e7b99a880e8
AutoHubEngineering <vetle_99@live.no>
2024-06-04 09:06:38 +0200

    Tested with servos
    This commit is tested with 5 servos and made the nescessary changes .
    Changes:
    Only using int in the map()
    Centering the servo by adding 90 degrees

--- Arduino_program/Arduino_program.ino
+++ Arduino_program/Arduino_program.ino
@@ -16,7 +16,12 @@ int servoPin[NumServos] = {2, 3, 4, 5, 6, 7};       //The pin to the servos on t
 int servoAngleCalc(int servoNumber){
   float servoRad = CurrentRad + (servoNumber * PhaseShift);     //Calculate the radian position to the servo
   double armPos = sin(servoRad);                                //Calculates the arm position. How far the arm are from the equalibrium line. 
-  int servoAngle = map(armPos, -1, 1, -Amplitude, Amplitude);   //Mapping from the sin value to the rigth degree for the servo
+  //Serial.println("ArmPos: " + String(armPos));
+  int armPosInt = armPos * 10000;
+  //Serial.println("ArmPosInt: " + String(armPosInt));
+  int servoAngle = map(armPosInt, -10000, 10000, -Amplitude, Amplitude);   //Mapping from the sin value to the rigth degree for the servo
+  //int servoAngle = (int)((armPos + 1) * (Amplitude / 2.0)); 
+  //Serial.println("Servo angle: " + String(servoAngle));
   return servoAngle;
 }
 
@@ -32,16 +37,20 @@ void setup() {
   for(int i = 0; i < NumServos; i++){
     servo[i].attach(servoPin[i]);
   }
+  Serial.println("Servo pins attached");
 
 
 }
 
 void loop() {
   CurrentRad = 2 * PI * millis() * Hz * 0.001;      //Sinus wave generator
+  Serial.println("Radian: " + String(CurrentRad));
 
   for(int i = 0; i < NumServos; i++){
-    servo[i].write(servoAngleCalc(i));
+    servo[i].write(servoAngleCalc(i)+90);
+    //Serial.println("write to a servo");
+    //delay(100);
   }
 
-  delay(10);
+  delay(5);
 }
